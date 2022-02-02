856b275a040d86dfe40a178711f27dead038faa7
Adrijana <adrijana.jovicic@gmail.com>
2022-02-02 16:16:20 +0100

    Loading Spinner

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Spinner from '../layout/Spinner';
 
 function UserResults() {
     const [users, setUsers] = useState([]);
@@ -11,11 +12,10 @@ function UserResults() {
     const fetchUsers = async () => {
         const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
             headers: {
-                Autorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
             }
         })
 
-        console.log(data);
         const data = await response.json();
 
         setUsers(data);
@@ -23,17 +23,16 @@ function UserResults() {
     }
 
     if (!loading) {
-        
-   
-    return (
-        <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-            {users.map((user) => (
-                <h3>{user.loading}</h3>
-            ))}
-        </div>
+
+        return (
+            <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+                {users.map((user) => (
+                    <h3>{user.login}</h3>
+                ))}
+            </div>
         )
     } else {
-       return <h3>Loading...</h3>
+        return <Spinner/>
     }
 }
 export default UserResults
