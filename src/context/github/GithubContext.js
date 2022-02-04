a4ebf38dd793ae361f891cbdc9701bb2ef2098ab
Adrijana <adrijana.jovicic@gmail.com>
2022-02-04 12:42:18 +0100

    User Search Component

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import { createContext, useReducer } from 'react';
 import githubReducer from './GithubReducer';
-=======
-import { createContext, useState } from 'react';
->>>>>>> 4c4e0779b9ddbd66d975c538589b13e0e56de9ed
 
 const GithubContext = createContext();
 
@@ -11,7 +7,6 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-<<<<<<< HEAD
     const initialState = {
         users: [],
         loading: false
@@ -22,12 +17,7 @@ export const GithubProvider = ({ children }) => {
     // Get initial users (testing purposes)
     const fetchUsers = async () => {
         setLoading();
-=======
-        const [users, setUsers] = useState([]);
-        const [loading, setLoading] = useState(true);
-
-        const fetchUsers = async() => {
->>>>>>> 4c4e0779b9ddbd66d975c538589b13e0e56de9ed
+        
             const response = await fetch(`${GITHUB_URL}/users`, {
                 headers: {
                     Authorization: `token ${GITHUB_TOKEN}`
@@ -36,7 +26,6 @@ export const GithubProvider = ({ children }) => {
 
             const data = await response.json();
 
-<<<<<<< HEAD
             dispatch({
                 type: 'GET_USERS',
                 payload: data,
@@ -51,16 +40,6 @@ export const GithubProvider = ({ children }) => {
         value={{
         users: state.users,
         loading: state.loading,
-=======
-            setUsers(data);
-            setLoading(false);
-        }
-
-    return <GithubContext.Provider
-        value={{
-        users,
-        loading,
->>>>>>> 4c4e0779b9ddbd66d975c538589b13e0e56de9ed
         fetchUsers,
     }}>
         {children}
